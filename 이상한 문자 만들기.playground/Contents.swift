49807604b86745576a35e1599c509cfa1337e8de
KangChangHyeok <dpkyle97@gmail.com>
2022-07-26 23:50:11 +0900

    이상한 문자 만들기

---
+++ 이상한 문자 만들기.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+func solution(_ s:String) -> String {
+    let arr = s.components(separatedBy: " ")
+    var result = ""
+    var toggle = true
+    for i in arr {
+        let arr2 = i.map { String($0)}
+        for i in arr2 {
+            if toggle {
+                result += i.uppercased()
+                toggle.toggle()
+            } else {
+                
+                result += i.lowercased()
+                toggle.toggle()
+            }
+        }
+        toggle = true
+        result += " "
+    }
+    let realResult = result.trimmingCharacters(in: [" "])
+    return realResult
+}
+
+solution("hello say SomeThinG")
